9de89a87e3a042264b6367171151ad7b7e7bbf02
codingfisch <l_fisc17@wwu.de>
2025-03-04 17:55:14 +0100

    Pong works

--- flashrl/envs/pong/cy_pong.pyx
+++ flashrl/envs/pong/cy_pong.pyx
@@ -11,7 +11,8 @@ cdef extern from 'pong.h':
         float *reward1
         float *done0
         float *done1
-        int size_x, size_y, t, paddle0_x, paddle0_y, paddle1_x, paddle1_y, ball_x, ball_y, ball_dx, ball_dy
+        int size_x, size_y, t, paddle0_x, paddle0_y, paddle1_x, paddle1_y, x, dx
+        float y, dy, max_dy
 
     void c_reset(Pong* env)
     void c_step(Pong* env)
@@ -21,17 +22,16 @@ cdef class CyPong:
     cdef:
         Pong* envs
         size_t n
-        int n_agents
 
     def __init__(self, char[:, :, :] obs, unsigned char[:] acts, float[:] rewards, float[:] dones, size_t n,
-                 int size_x, int size_y):
-        self.envs = <Pong*>calloc(n, sizeof(Pong))
+                 int size_x, int size_y, float max_dy):
+        self.envs = <Pong*>calloc(n // 2, sizeof(Pong))
         self.n = n
         cdef int i
         for i in range(0, self.n, 2):
             self.envs[i//2] = Pong(&obs[i, 0, 0], &obs[i+1, 0, 0], &acts[i], &acts[i+1], &rewards[i], &rewards[i+1],
-                                   &dones[i], &dones[i+1], size_x=size_x, size_y=size_y, t=0, paddle0_x=0, paddle0_y=0,
-                                   paddle1_x=0, paddle1_y=0, ball_x=0, ball_y=0, ball_dx=0, ball_dy=0)
+                                   &dones[i], &dones[i+1], size_x=size_x, size_y=size_y, max_dy=max_dy, t=0,
+                                   paddle0_x=0, paddle0_y=0, paddle1_x=0, paddle1_y=0, x=0, y=0, dx=0, dy=0)
 
     def reset(self, seed=None):
         if seed is not None:
--- flashrl/envs/pong/pong.h
+++ flashrl/envs/pong/pong.h
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -8,49 +9,66 @@ typedef struct {
     char *obs0, *obs1;
     unsigned char *act0, *act1;
     float *reward0, *reward1, *done0, *done1;
-    int size_x, size_y, t, paddle0_x, paddle0_y, paddle1_x, paddle1_y, ball_x, ball_y, ball_dx, ball_dy;
+    int size_x, size_y, t, paddle0_x, paddle0_y, paddle1_x, paddle1_y, x, dx;
+    float y, dy, max_dy;
 } Pong;
+
+void set_obs(Pong* env, char paddle, char ball) {
+    for (int i = -1; i < 2; i++) {
+        if (env->paddle0_y + i >= 0 && env->paddle0_y + i <= env->size_y - 1) {
+            env->obs0[(env->size_x - 1) - env->paddle0_x + (env->paddle0_y + i) * env->size_x] = paddle;
+            env->obs1[env->paddle0_x + (env->paddle0_y + i) * env->size_x] = paddle;
+        }
+        if (env->paddle1_y + i >= 0 && env->paddle1_y + i <= env->size_y - 1) {
+            env->obs0[(env->size_x - 1) - env->paddle1_x + (env->paddle1_y + i) * env->size_x] = paddle;
+            env->obs1[env->paddle1_x + (env->paddle1_y + i) * env->size_x] = paddle;
+        }
+    }
+    env->obs0[(env->size_x - 1) - env->x + (int)(roundf(env->y)) * env->size_x] = ball;
+    env->obs1[env->x + (int)(roundf(env->y)) * env->size_x] = ball;
+}
 
 void c_reset(Pong* env) {
     env->t = 0;
     memset(env->obs0, 0, env->size_x * env->size_y);
     memset(env->obs1, 0, env->size_x * env->size_y);
-    env->ball_x = env->size_x / 2;
-    env->ball_y = env->size_y / 2;
-    env->ball_dx = (rand() % 2) ? 1 : -1;
-    env->ball_dy = (rand() % 2) ? 1 : -1;
+    env->x = env->size_x / 2;
+    env->y = rand() % (env->size_y - 1);
+    env->dx = (rand() % 2) ? 1 : -1;
+    env->dy = 2.0f * ((float)rand() / RAND_MAX) - 1.0f;
     env->paddle0_x = 0;
     env->paddle1_x = env->size_x - 1;
     env->paddle0_y = env->paddle1_y = env->size_y / 2;
-    env->reward0[0] = env->reward1[0] = 0;
-    env->done0[0] = env->done1[0] = 0;
+    set_obs(env, PADDLE, BALL);
 }
 
 void c_step(Pong* env) {
-    env->obs0[(env->size_x - 1) - env->paddle0_x + env->paddle0_y * env->size_x] = 0;
-    env->obs0[(env->size_x - 1) - env->paddle1_x + env->paddle1_y * env->size_x] = 0;
-    env->obs1[env->paddle0_x + env->paddle0_y * env->size_x] = 0;
-    env->obs1[env->paddle1_x + env->paddle1_y * env->size_x] = 0;
-    env->obs0[(env->size_x - 1) - env->ball_x + env->ball_y * env->size_x] = 0;
-    env->obs1[env->ball_x + env->ball_y * env->size_x] = 0;
+    env->reward0[0] = env->reward1[0] = 0;
+    env->done0[0] = env->done1[0] = 0;
+    set_obs(env, 0, 0);
     if (env->act0[0] == UP && env->paddle0_y > 0) env->paddle0_y--;
     if (env->act0[0] == DOWN && env->paddle0_y < env->size_y - 2) env->paddle0_y++;
     if (env->act1[0] == UP && env->paddle1_y > 0) env->paddle1_y--;
     if (env->act1[0] == DOWN && env->paddle1_y < env->size_y - 2) env->paddle1_y++;
-    env->ball_x += env->ball_dx;
-    env->ball_y += env->ball_dy;
-    if (env->ball_y <= 0 || env->ball_y >= env->size_y - 1) env->ball_dy = -env->ball_dy;
-    if (env->ball_x == 1 && env->ball_y >= env->paddle0_y && env->ball_y <= env->paddle0_y + 1)
-        env->ball_dx = -env->ball_dx;
-    if (env->ball_x == env->size_x - 2 && env->ball_y >= env->paddle1_y && env->ball_y <= env->paddle1_y + 1)
-        env->ball_dx = -env->ball_dx;
-    if (env->ball_x <= 0) { env->reward0[0]--; env->reward1[0]++; env->done0[0]++; env->done1[0]++; c_reset(env); }
-    if (env->ball_x >= env->size_x - 1) { env->reward0[0]++; env->reward1[0]--;  env->done0[0]++; env->done1[0]++; c_reset(env); }
-    env->obs0[(env->size_x - 1) - env->paddle0_x + env->paddle0_y * env->size_x] = PADDLE;
-    env->obs0[(env->size_x - 1) - env->paddle1_x + env->paddle1_y * env->size_x] = PADDLE;
-    env->obs1[env->paddle0_x + env->paddle0_y * env->size_x] = PADDLE;
-    env->obs1[env->paddle1_x + env->paddle1_y * env->size_x] = PADDLE;
-    env->obs0[(env->size_x - 1) - env->ball_x + env->ball_y * env->size_x] = BALL;
-    env->obs1[env->ball_x + env->ball_y * env->size_x] = BALL;
+    env->dy = fminf(fmaxf(env->dy, -env->max_dy), env->max_dy);
+    env->x += env->dx;
+    env->y += env->dy;
+    env->y = fminf(fmaxf(env->y, 0.f), env->size_y - 1.f);
+    if (env->y <= 0 || env->y >= env->size_y - 1) env->dy = -env->dy;
+    if (env->x == 1 && env->y >= env->paddle0_y - 1 && env->y <= env->paddle0_y + 1) {
+        env->dx = -env->dx;
+        env->dy += env->y - env->paddle0_y;
+    }
+    if (env->x == env->size_x - 2 && env->y >= env->paddle1_y - 1 && env->y <= env->paddle1_y + 1) {
+        env->dx = -env->dx;
+        env->dy += env->y - env->paddle1_y;
+    }
+    if (env->x == 0 || env->x == env->size_x - 1) {
+        env->reward1[0] = 2 * (float)(env->x == 0) - 1.f;
+        env->reward0[0] = -env->reward1[0];
+        env->done0[0] = env->done1[0] = 1.f;
+        c_reset(env);
+    }
+    set_obs(env, PADDLE, BALL);
     env->t++;
 }
