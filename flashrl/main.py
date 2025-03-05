c3f7685f77429b90611465696c2c8ccad01b47dc
codingfisch <l.fisch@uni-muenster.de>
2025-03-05 13:54:28 +0100

    Reapply "More refactoring"
    This reverts commit 1fe0587e7896dc0a7f5b889da4a97626906ea6a1.

--- flashrl/main.py
+++ flashrl/main.py
@@ -7,33 +7,34 @@ from .models import LSTMPolicy
 
 
 class Learner:
-    def __init__(self, env, model=None, precision='medium'):
+    def __init__(self, env, model=None):
         self.env = env
         self.model = LSTMPolicy(self.env).to(self.env.device, self.env.dtype) if model is None else model
         self._data, self._np_data = None, None
-        torch.set_float32_matmul_precision(precision)
+
+    @property
+    def scalar_data_keys(self):
+        return [k for k, v in self._data.items() if v.ndim == 2]
 
     def fit(self, iters=40, steps=16, lr=.01, anneal_lr=True, log=False, pbar_desc=None, target_kl=None, **hparams):
         self.setup_data(steps)
-        metrics_curves = []
+        curves = []
         logger = SummaryWriter() if log else None
         opt = torch.optim.Adam(self.model.parameters(), lr=lr, eps=1e-5)
         pbar = tqdm(range(iters), total=iters)
-        hc = torch.zeros((2, self.env.n_envs, self.model.lstm.hidden_size), dtype=self.env.dtype, device=self.env.device)
         for i in pbar:
-            t0 = time()
             opt.param_groups[0]['lr'] = lr * (1 - i / iters) if anneal_lr else lr
-            self.rollout(steps, state=(hc[0], hc[1]))
+            self.rollout(steps)
             metrics = ppo(self.model, opt, **self._data, **hparams)
-            pbar.set_description(f'{pbar_desc}: {self._data[pbar_desc+"s"].mean():.3f}')
-            pbar.set_postfix_str(f'{1e-6 * self.env.n_envs * steps / (time() - t0):.1f}million steps/s')
+            pbar.set_postfix_str(f'{1e-6 * self.env.n_envs * steps / (time() - pbar.last_print_t):.1f}million steps/s')
+            pbar.set_description(f'{pbar_desc}: {self._data[pbar_desc + "s"].mean():.3f}')
             if log:
                 for k, v in metrics.items(): logger.add_scalar(k, v, global_step=i)
                 for name, param in self.model.named_parameters(): logger.add_histogram(name, param, global_step=i)
-            metrics_curves.append(metrics)
+            curves.append(metrics)
             if target_kl is not None:
                 if metrics['kl'] > target_kl: break
-        return {k: [m[k].item() for m in metrics_curves] for k in metrics_curves[0]}
+        return {k: [m[k].item() for m in curves] for k in curves[0]}
 
     def setup_data(self, duration):
         values = torch.empty((self.env.n_envs, duration), dtype=self.env.dtype, device=self.env.device)
@@ -59,13 +60,12 @@ class Learner:
         return torch.from_numpy(x).to(device=device or self.env.device, dtype=dtype or self.env.dtype, **kwargs)
 
 
-def ppo(model, opt, obs, values, acts, logprobs, rewards, dones, bs=8192, gamma=.99, gae_lambda=.95,
-        clip_coef=.1, value_coef=.5, value_clip_coef=.1, entropy_coef=.01, max_grad_norm=.5, norm_adv=True):
+def ppo(model, opt, obs, values, acts, logprobs, rewards, dones, bs=8192, gamma=.99, gae_lambda=.95, clip_coef=.1,
+        value_coef=.5, value_clip_coef=.1, entropy_coef=.01, max_grad_norm=.5, norm_adv=True, state=None):
     advs = get_advantages(values, rewards, dones, gamma=gamma, gae_lambda=gae_lambda)
     obs, values, acts, logprobs, advs = [xs.view(-1, bs, *xs.shape[2:]) for xs in [obs, values, acts, logprobs, advs]]
     returns = advs + values
     metrics, metric_keys = [], ['loss', 'policy_loss', 'value_loss', 'entropy_loss', 'kl', 'clip_frac']
-    state = None
     for o, old_value, act, old_logp, adv, ret in zip(obs, values, acts, logprobs, advs, returns):
         _, logp, entropy, value, state = model(o, state=state, act=act)
         state = (state[0].detach(), state[1].detach())
--- train.py
+++ train.py
@@ -1,11 +1,12 @@
 import flashrl as frl
 frl.set_seed(SEED:=1)
 
-DEVICE, DTYPE = 'cuda', 'bfloat16'  # for CPU use 'cpu', 'float32'
-learn = frl.Learner(env=frl.envs.Pong(n_envs=16*1024, device=DEVICE, dtype=DTYPE).reset(SEED))
-# learn.model = frl.LSTMPolicy(learn.env, n_hidden=128).to(DEVICE, DTYPE)  # uncomment to modify model (e.g. n_hidden)
-curves = learn.fit(40, steps=16, lr=1e-2, log=False, pbar_desc='done')  # modify hparams here (e.g. gamma)
-learn.env.close()
-frl.print_curves(curves, keys='loss')
-frl.render_ascii(learn._data['obs'][0], obs_max=learn.env.obs_max, fps=4)#, data=data)
-frl.render_gif('gameplay.gif', learn._data['obs'][0], obs_max=learn.env.obs_max, fps=2)#, data=data)
+env = frl.envs.Pong(n_envs=2**14, device='cuda', dtype='bfloat16')  # for CPU use 'cpu', 'float32'
+learn = frl.Learner(env=env.reset(SEED))
+#learn.model = frl.LSTMPolicy(learn.env, n_hidden=128).to(env.device, env.dtype)  # uncomment to modify model
+curves = learn.fit(40, steps=16, pbar_desc='done')  # modify hparams here (e.g. lr, gamma...)
+frl.print_ascii_curves(curves, keys='loss')
+frl.render_ascii(learn, fps=8)
+frl.render_gif('gameplay.gif', learn, fps=4)
+#frl.print_table(learn)
+env.close()
